6e8e63016ef31a226a11f99fff0a1466e5dd38a6
patricia6596 <59688392+patricia6596@users.noreply.github.com>
2020-11-30 11:09:36 +0100

    probando

--- conexion.php
+++ conexion.php
@@ -1,28 +1,29 @@
  <?php
-    class Conexion {
-        private static $_handle = null;
-        private static $_dsn = "mysql:dbname=mialdea;host=localhost";
-        private static $_user = "jap";
-        private static $_password = "mialdea";
-        private static $_options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+    class Db {
+        private static $conexion = null;
+        private static $dbname = "mysql:dbname=registro;host=localhost";
+        private static $usuario = "jap";
+        private static $password = "mialdea";
+        private static $pdo_options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+        private $db;
         private function __construct(){
-            $this->conectar();
+            $db=$this->conectar();
          }
         function conectar(){
-            if ( is_null( self::$_handle ) ) {
+            if ( is_null( self::$conexion ) ) {
                 try {
-                    self::$_handle = new PDO( 
-                        self::$_dsn, self::$_user, self::$_password, self::$_options
+                    self::$conexion = new PDO( 
+                        self::$dbname, self::$usuario, self::$password, self::$pdo_options
                     );
                 } catch ( PDOException $error ){
                     die ( $error->getMessage() );
                 }
             }
-            return self::$_handle;
+            return self::$conexion;
         }
-        public function consultar($sql){
-            $this->stmt=mysql_query($sql,$this->link);
-            return $this->stmt;
+        public function consultar(){
+            $select = $this->db -> prepare( 'select * from usuarios');
+            return $select -> execute();
         }
         public function insertar($sql){
             $this->stmt=mysql_query($sql,$this->link);
