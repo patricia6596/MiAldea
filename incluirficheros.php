f075e5ce7ca1ab98b1c85972a1da5fe89ed0b0c0
patricia6596 <59688392+patricia6596@users.noreply.github.com>
2020-11-30 11:12:21 +0100

    incluirficheros

---
+++ incluirficheros.php
@@ -0,0 +1,10 @@
+<?php
+    //Fichero para incluir ficheros
+    require_once 'conexion.php';
+    require_once 'personajes.php';
+    require_once 'batalla.php';
+    require_once 'Guerrero.php';
+    require_once 'arquero.php';
+    require_once 'objetos.php';
+    
+?>
