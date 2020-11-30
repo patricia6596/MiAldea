31d34f1c0875753bfc0c3910992bd686aa9f19dc
AngelMolSan <46252424+AngelMolSan@users.noreply.github.com>
2020-11-30 12:11:22 +0100

    Add files via upload

--- personajes.php
+++ personajes.php
@@ -2,7 +2,7 @@
     class Personaje {
         protected $nombre;
         protected $tipo;
-        private $puntos; 
+        private $puntos;
         protected $arma=array();
 
         public function __construct($nom){
@@ -14,19 +14,19 @@
             if($resultado==1){
                 //Gana
                 $this->puntos=$this->puntos+3;
-                echo "Has ganado recibes 3 puntos";
+                echo "Has ganado, recibes 3 puntos";
             } elseif ($resultado==2){
                 //Empata
                 $this->puntos=$this->puntos+1;
-                echo "Has empatado recibes 1 puntos";
+                echo "Has empatado, recibes 1 puntos";
             } else {
                 //Pierde
                 $this->puntos=$this->puntos-1;
                 echo "Has perdido te quitan 1 puntos";
             }
         }
-        public function imprimirPuntos(){
-            echo $this->puntos;
+        public function devolverPuntos(){
+            return $this->puntos;
         }
         public function conseguirArma($arma){
             //Añadira un arma al array armas;
@@ -37,4 +37,3 @@
         }
     }
 ?>
- 
