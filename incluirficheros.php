051d98fd5c206e9eabfe1b5a76bd4c5513203401
patricia6596 <59688392+patricia6596@users.noreply.github.com>
2020-12-02 19:07:51 +0100

    *

--- incluirficheros.php
+++ incluirficheros.php
@@ -4,7 +4,7 @@
     require_once 'conexion.php';
     require_once 'personajes.php';
     require_once 'batalla.php';
-    require_once 'Guerrero.php';
+    require_once 'guerrero.php';
     require_once 'arquero.php';
     require_once 'mago.php';
     require_once 'objetos.php';
